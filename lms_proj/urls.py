958ca029f635cb585c829f38bd200af2d05fb500
mahmoudtokura <mahmoudbintokura@gmail.com>
2020-03-15 13:07:17 +0100

    Setup the home page and uer authentication

--- lms_proj/urls.py
+++ lms_proj/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
